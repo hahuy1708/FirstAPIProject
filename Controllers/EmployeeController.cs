d5401579e3db033ca5d2ab020ebad4c28bc71ff4
hahuy1708 <hahuy17082005@gmail.com>
2025-05-09 09:32:18 +0700

    add controller

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAPIProject.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Returning from EmployeeController GET Method";
+        }
+        [HttpGet]
+        public string GetEmployee()
+        {
+            return "Returning from EmployeeController GETEMPLOYEE Method";
+        }
+    }
+}
