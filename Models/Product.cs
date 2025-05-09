7b33a6578199b3e864ebffb6a37319dee1bd3a83
hahuy1708 <hahuy17082005@gmail.com>
2025-05-09 12:48:45 +0700

    add Product Models

---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+namespace FirstAPIProject.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Range(0.01, 10000.00)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+    }
+}
